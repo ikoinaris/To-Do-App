af19cc9ddb873f507f75cdd002c926ea2a492a2e
Ioannis Koinaris (ext.) <Ioannis.Koinaris@adesson.de>
2021-10-22 10:46:05 +0200

    first form element created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Form from "./components/Form"
 
 const apptTitle = "To-Do App"
 
 const App = () => {
     return <div className="ui container center aligned">
         <h1>{apptTitle}</h1>
+        <Form />
     </div>
 };
 
