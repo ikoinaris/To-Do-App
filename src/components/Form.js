8536b489fd90a9b67610b336d18f8655dcdaab07
Ioannis Koinaris (ext.) <Ioannis.Koinaris@adesson.de>
2021-10-22 11:09:53 +0200

    style modifications

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Form = () => {
     return (
         <form className="ui form">
-            <div className="ui grid">
+            <div className="ui grid center aligned">
                 <div className="row">
                     <div className="column five wide">
-                        <input type="text"/>
+                        <input type="text" placeholder="Enter To-Do..."/>
                     </div>
                     <div className="column one wide">
-                        <button type="submit">Add</button>
+                        <button type="submit" className="ui button circular icon green"><i className="plus icon white"></i></button>
                     </div>
                 </div>
             </div>
