0d2a518ce4dac6f50425844947916664adcb777c
Ioannis Koinaris (ext.) <Ioannis.Koinaris@adesson.de>
2021-10-22 11:18:48 +0200

    section component created to seperate main components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Form from "./components/Form"
+import Section from './components/Section';
 
 const apptTitle = "To-Do App"
 
 const App = () => {
     return <div className="ui container center aligned">
-        <h1>{apptTitle}</h1>
-        <Form />
+        <Section>
+            <h1>{apptTitle}</h1>
+        </Section>
+        <Section>
+            <Form />
+        </Section>
     </div>
 };
 
